80e91b7e14bd21823961ca662faf5f31f6060783
ethantseng <tseng8851@mydusd.org>
2025-11-07 11:10:54 -0800

    Create TraverseBackwards.java

---
+++ TraverseBackwards.java
@@ -0,0 +1,12 @@
+/*
+ * Activity 3.1.2
+ */
+public class TraverseBackwards
+{
+  public static void main(String[] args)
+  {    
+    int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
+                   
+    /* your code here */
+  }
+}
