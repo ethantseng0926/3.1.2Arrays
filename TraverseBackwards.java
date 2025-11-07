2ac61afe6465a3f23102c41bb5d4f91baee7e091
ethantseng <tseng8851@mydusd.org>
2025-11-07 11:11:54 -0800

    Update TraverseBackwards.java

--- TraverseBackwards.java
+++ TraverseBackwards.java
@@ -6,7 +6,10 @@ public class TraverseBackwards
   public static void main(String[] args)
   {    
     int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
-                   
-    /* your code here */
+    
+    for (int i = goals.length - 1; i >= 0; i--) 
+    {
+      System.out.println("Goals in game " + (i + 1) + ": " + goals[i]);
+    }
   }
 }
